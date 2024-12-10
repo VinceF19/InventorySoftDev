7f79bcf6865a0bd5a7651ddf42976e0ba1dd1802
VanneDelaRosa <115532511+VanneDelaRosa@users.noreply.github.com>
2024-12-10 22:29:40 +0800

    recent

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inventory',
-    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -119,8 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+                    
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "inventory/static",
+],
